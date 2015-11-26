1b6a19e51d66ff5813e1c74e8325f10dd795eb8d
Christopher R. Palmer <crpalmer@gmail.com>
2015-11-26 06:53:50 -0500

    wip warnings
    Change-Id: Iba50ee8fa083134d6db7474987158ff31fd289aa

--- arch/x86/platform/intel-mid/device_libs/pci/platform_sdhci_pci.c
+++ arch/x86/platform/intel-mid/device_libs/pci/platform_sdhci_pci.c
@@ -273,6 +273,7 @@ static int mrfl_flis_slew_change(void __iomem *flis_addr, int slew)
 	return 0;
 }
 
+#if 0
 static int mrfl_flis_check(void __iomem *addr,
 unsigned int host_clk, unsigned int clk)
 {
@@ -287,6 +288,7 @@ unsigned int host_clk, unsigned int clk)
 
 	return ret;
 }
+#endif
 
 static int mrfl_flis_dump(void __iomem *addr)
 {
